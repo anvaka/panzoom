64a48533ad7dee1bd5724bbb886733dc303fd3c7
Niek Kruse <niekkruse70@gmail.com>
2018-12-13 22:12:03 +0100

    Add typescript typings

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,56 @@
 declare module "panzoom" {
-    export default function createPanZoom(domElement: HTMLElement | SVGElement, options: object): void
+  interface Bounds {
+    left: number;
+    top: number;
+    right: number;
+    bottom: number;
+  }
+
+  export interface PanZoomOptions {
+    filterKey?: () => boolean;
+    bounds?: boolean | Bounds;
+    realPinch?: boolean;
+    maxZoom?: number;
+    minZoom?: number;
+    boundsPadding?: number;
+    zoomDoubleClickSpeed?: number;
+    zoomSpeed?: number;
+    beforeWheel?: (e: WheelEvent) => void;
+    autocenter?: boolean;
+    onTouch?: (e: TouchEvent) => void;
+    onDoubleClick?: (e: Event) => void;
+    smoothScroll?: boolean;
+    controller?: SVGElement | HTMLElement;
+  }
+
+  export interface PanZoom {
+    dispose: () => void;
+    moveBy: (dx: number, dy: number, smooth: boolean) => void;
+    moveTo: (x: number, y: number) => void;
+    centerOn: (ui: any) => void;
+    zoomTo: (clientX: number, clientY: number, scaleMultiplier: number) => void;
+    zoomAbs: (clientX: number, clientY: number, zoomLevel: number) => void;
+    smoothZoom: (
+      clientX: number,
+      clientY: number,
+      scaleMultiplier: number
+    ) => void;
+    getTransform: () => {
+      x: number;
+      y: number;
+      scale: number;
+    };
+    showRectangle: (rect: ClientRect) => void;
+    pause: () => void;
+    resume: () => void;
+    isPaused: () => boolean;
+    on: <T>(eventName: string, handler: (e: T) => void) => void;
+    off: (eventName, handler: Function) => void;
+    fire: (eventName) => void;
+  }
+
+  export default function createPanZoom(
+    domElement: HTMLElement | SVGElement,
+    Options: PanZoomOptions
+  ): PanZoom;
 }

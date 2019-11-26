94492b7abe333e547d0ddb1a5fb8668a63b7f47a
Andriy Kashcha <anvaka@gmail.com>
2019-11-25 20:41:17 -0800

    removed window dependency from kinetic

--- dist/panzoom.js
+++ dist/panzoom.js
@@ -1064,6 +1064,7 @@ function makeDomController(domElement, options) {
 }
 
 },{}],3:[function(require,module,exports){
+(function (global){
 /**
  * Allows smooth kinetic scrolling of the surface
  */
@@ -1072,46 +1073,51 @@ module.exports = kinetic;
 function kinetic(getPoint, scroll, settings) {
   if (typeof settings !== 'object') {
     // setting could come as boolean, we should ignore it, and use an object.
-    settings = {}
+    settings = {};
   }
 
-  var minVelocity = (typeof settings.minVelocity === 'number') ? settings.minVelocity : 5
-  var amplitude = (typeof settings.amplitude === 'number') ? settings.amplitude : 0.25
+  var minVelocity = typeof settings.minVelocity === 'number' ? settings.minVelocity : 5;
+  var amplitude = typeof settings.amplitude === 'number' ? settings.amplitude : 0.25;
+  var cancelAnimationFrame = typeof settings.cancelAnimationFrame === 'function' ? settings.cancelAnimationFrame : getCancelAnimationFrame();
+  var requestAnimationFrame = typeof settings.requestAnimationFrame === 'function' ? settings.requestAnimationFrame : getRequestAnimationFrame();
 
-  var lastPoint
-  var timestamp
-  var timeConstant = 342
+  var setInterval = typeof settings.setInterval === 'function' ? settings.setInterval : global.setInterval;
+  var clearInterval = typeof settings.clearInterval === 'function' ? settings.clearInterval : global.clearInterval;
 
-  var ticker
+  var lastPoint;
+  var timestamp;
+  var timeConstant = 342;
+
+  var ticker;
   var vx, targetX, ax;
   var vy, targetY, ay;
 
-  var raf
+  var raf;
 
   return {
     start: start,
     stop: stop,
     cancel: dispose
-  }
+  };
 
   function dispose() {
-    window.clearInterval(ticker)
-    window.cancelAnimationFrame(raf)
+    clearInterval(ticker);
+    cancelAnimationFrame(raf);
   }
 
   function start() {
-    lastPoint = getPoint()
+    lastPoint = getPoint();
 
-    ax = ay = vx = vy = 0
-    timestamp = new Date()
+    ax = ay = vx = vy = 0;
+    timestamp = new Date();
 
-    window.clearInterval(ticker)
-    window.cancelAnimationFrame(raf)
+    clearInterval(ticker);
+    cancelAnimationFrame(raf);
 
     // we start polling the point position to accumulate velocity
     // Once we stop(), we will use accumulated velocity to keep scrolling
     // an object.
-    ticker = window.setInterval(track, 100);
+    ticker = setInterval(track, 100);
   }
 
   function track() {
@@ -1119,72 +1125,85 @@ function kinetic(getPoint, scroll, settings) {
     var elapsed = now - timestamp;
     timestamp = now;
 
-    var currentPoint = getPoint()
+    var currentPoint = getPoint();
 
-    var dx = currentPoint.x - lastPoint.x
-    var dy = currentPoint.y - lastPoint.y
+    var dx = currentPoint.x - lastPoint.x;
+    var dy = currentPoint.y - lastPoint.y;
 
-    lastPoint = currentPoint
+    lastPoint = currentPoint;
 
-    var dt = 1000 / (1 + elapsed)
+    var dt = 1000 / (1 + elapsed);
 
     // moving average
-    vx = 0.8 * dx * dt + 0.2 * vx
-    vy = 0.8 * dy * dt + 0.2 * vy
+    vx = 0.8 * dx * dt + 0.2 * vx;
+    vy = 0.8 * dy * dt + 0.2 * vy;
   }
 
   function stop() {
-    window.clearInterval(ticker);
-    window.cancelAnimationFrame(raf)
+    clearInterval(ticker);
+    cancelAnimationFrame(raf);
 
-    var currentPoint = getPoint()
+    var currentPoint = getPoint();
 
-    targetX = currentPoint.x
-    targetY = currentPoint.y
-    timestamp = Date.now()
+    targetX = currentPoint.x;
+    targetY = currentPoint.y;
+    timestamp = Date.now();
 
     if (vx < -minVelocity || vx > minVelocity) {
-      ax = amplitude * vx
-      targetX += ax
+      ax = amplitude * vx;
+      targetX += ax;
     }
 
     if (vy < -minVelocity || vy > minVelocity) {
-      ay = amplitude * vy
-      targetY += ay
+      ay = amplitude * vy;
+      targetY += ay;
     }
 
-    raf = window.requestAnimationFrame(autoScroll);
+    raf = requestAnimationFrame(autoScroll);
   }
 
   function autoScroll() {
-    var elapsed = Date.now() - timestamp
+    var elapsed = Date.now() - timestamp;
 
-    var moving = false
-    var dx = 0
-    var dy = 0
+    var moving = false;
+    var dx = 0;
+    var dy = 0;
 
     if (ax) {
-      dx = -ax * Math.exp(-elapsed / timeConstant)
+      dx = -ax * Math.exp(-elapsed / timeConstant);
 
-      if (dx > 0.5 || dx < -0.5) moving = true
-      else dx = ax = 0
+      if (dx > 0.5 || dx < -0.5) moving = true;
+      else dx = ax = 0;
     }
 
     if (ay) {
-      dy = -ay * Math.exp(-elapsed / timeConstant)
+      dy = -ay * Math.exp(-elapsed / timeConstant);
 
-      if (dy > 0.5 || dy < -0.5) moving = true
-      else dy = ay = 0
+      if (dy > 0.5 || dy < -0.5) moving = true;
+      else dy = ay = 0;
     }
 
     if (moving) {
-      scroll(targetX + dx, targetY + dy)
-      raf = window.requestAnimationFrame(autoScroll);
+      scroll(targetX + dx, targetY + dy);
+      raf = requestAnimationFrame(autoScroll);
     }
   }
-
 }
 
+function getCancelAnimationFrame() {
+  if (typeof global.cancelAnimationFrame === 'function') return global.cancelAnimationFrame;
+
+  return clearTimeout;
+}
+
+function getRequestAnimationFrame() {
+  if (typeof global.requestAnimationFrame === 'function') return global.requestAnimationFrame;
+
+  return function (handler) {
+    return setTimeout(handler, 16);
+  }
+}
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],4:[function(require,module,exports){
 module.exports = makeSvgController
 
